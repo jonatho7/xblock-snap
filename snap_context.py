3ddb794d2d534cf758ecc5f558c748521dc182f3
Jonathon Hellmann <jonatho7@vt.edu>
2015-03-02 22:58:08 -0500

    Cleaned up the code.

--- snap_context.py
+++ snap_context.py
@@ -4,9 +4,10 @@ import requests
 from urlparse import urlparse
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer, String
+from xblock.fields import Scope, String
 from xblock.fragment import Fragment
 from xml.sax.saxutils import unescape
+
 
 class SnapContextBlock(XBlock):
     """
@@ -25,7 +26,7 @@ class SnapContextBlock(XBlock):
         """
 
         # Get the url where the snap json data is at. Unescape the xml-escaped url.
-        #   XML-Escaped URL: https://drive.google.com/uc?export=download&amp;id=0B-WWj_i0WSomWFlvcURmaExsbWM
+        # XML-Escaped URL: https://drive.google.com/uc?export=download&amp;id=0B-WWj_i0WSomWFlvcURmaExsbWM
         #   XML-UnEscaped URL: https://drive.google.com/uc?export=download&id=0B-WWj_i0WSomWFlvcURmaExsbWM
         xml_unescaped_url = unescape(self.problem_data_url)
 
@@ -36,55 +37,22 @@ class SnapContextBlock(XBlock):
         snap_problem_url = json_data['snap_problem_url']
         snap_problem_posttext = json_data['snap_problem_posttext']
 
-
         # Load the HTML fragment from within the package and fill in the template
         html_str = pkg_resources.resource_string(__name__, "static/html/snap_context.html")
         frag = Fragment(unicode(html_str).format(self=self,
                                                  snap_problem_pretext=snap_problem_pretext,
                                                  snap_problem_url=snap_problem_url,
                                                  snap_problem_posttext=snap_problem_posttext))
-
         return frag
-
-    def get_embed_code_for_url(self, url):
-        """
-        Get the code to embed from the oEmbed provider.
-        """
-        hostname = url and urlparse(url).hostname
-        # Check that the provider is supported
-        if hostname == 'vimeo.com':
-            oembed_url = 'http://vimeo.com/api/oembed.json'
-        else:
-            return hostname, '<p>Unsupported video provider ({0})</p>'.format(hostname)
-
-        params = {
-            'url': url,
-            'format': 'json',
-            'maxwidth': self.maxwidth,
-            'maxheight': self.maxheight,
-            'api': True
-        }
-
-        try:
-            r = requests.get(oembed_url, params=params)
-            r.raise_for_status()
-        except Exception as e:
-            return hostname, '<p>Error getting video from provider ({error})</p>'.format(error=e)
-        response = r.json()
-
-        return hostname, response['html']
 
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
         return [
             ("snap_context",
-            """
+             """
             <vertical_demo>
                 <snap_context problem_data_url="https://drive.google.com/uc?export=download&amp;id=0B-WWj_i0WSomWFlvcURmaExsbWM" />
-                <html_demo><div>Rate the video:</div></html_demo>
-                <thumbs />
             </vertical_demo>
             """)
         ]
-
